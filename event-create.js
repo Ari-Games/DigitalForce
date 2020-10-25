2b45616dbaef8c19013542d900bf937eaab31d05
Aramcho <osipyan.aram2017@ya.ru>
2020-10-25 05:41:05 +0300

    added event creating

---
+++ event-create.js
@@ -0,0 +1,31 @@
+$(document).ready(function() { 
+    let j =  localStorage.getItem("admin-jwt");
+
+    $("#event-card").submit(function(e) {
+        var $input = $("#uploadimage");
+        var fd = new FormData;
+    
+        fd.append('event[picture]', $input.prop('files')[0]);
+        fd.append("event[tag]",$("#tag").val());
+        fd.append("event[name]",$("#name").val());
+        fd.append("event[description]",$("#description").val());
+        fd.append("event[time]",$("#time").val());
+        fd.append("event[date]",$("#date").val());
+        fd.append("event[user_id]",1);
+        $.ajax({
+            url: 'http://178.154.255.209:3005/events',
+            data: fd,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            beforeSend:function(xhr){
+                xhr.setRequestHeader("Authorization","Bearer "+j);     
+                    },
+            success: function (data) {
+                alert(data);
+            }
+        });
+        e.preventDefault();
+       
+    })
+ });
